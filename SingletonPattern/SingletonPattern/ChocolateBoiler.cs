f5ccb8c42261137fa8bf68c6bb8787ef8270f38b
ndTh4ng <thangxacam@gmail.com>
2021-11-05 21:58:50 +0700

    Singleton Pattern

---
+++ SingletonPattern/SingletonPattern/ChocolateBoiler.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    class ChocolateBoiler
+    {
+        private bool empty;
+        private bool boiled;
+
+        // Tạo một instance duy nhất
+        private static ChocolateBoiler uniqueInstance;
+
+        // Triển khai một private constructor để ngăn các lớp khác khai báo nhiều hơn một instance.
+        private ChocolateBoiler()
+        {
+            empty = true;
+            boiled = false;
+        }
+
+        // Khởi tạo phương thức static để có thể truy xuất đến phương thức này từ bất cứ nơi nào
+        public static ChocolateBoiler getInstance()
+        {
+            if (uniqueInstance == null)
+            {
+                uniqueInstance = new ChocolateBoiler();
+            }
+            // Không tạo thêm mà chỉ trả về một instance duy nhất
+            return uniqueInstance;
+        }
+
+        public void Fill()
+        {
+            if (IsEmpty())
+            {
+                empty = false;
+                boiled = false;
+                Console.WriteLine("Fill the boiler with milk/chocolate mixture");
+            }
+            else
+            {
+                Console.WriteLine("The boiler is already filled!");
+            }
+        }
+
+        public void Drain()
+        {
+            if (!IsEmpty() && IsBoiled())
+            {               
+                empty = true;
+                Console.WriteLine("Drain the boiler");
+            }
+        }
+
+        public void Boil()
+        {
+            if (!IsEmpty() && !IsBoiled())
+            {
+                boiled = true;
+                Console.WriteLine("Boiled the mixture");
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            return empty;
+        }
+
+        public bool IsBoiled()
+        {
+            return boiled;
+        }
+    }
+}
---
+++ SingletonPattern/SingletonPattern/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Không thể tạo bằng new ( = new ChocolateBoiler()) vì constructor khởi tạo là private
+            // Lấy đối tượng thông qua phương thức getIntance();
+            ChocolateBoiler boiler = ChocolateBoiler.getInstance();
+            ChocolateBoiler boiler2 = ChocolateBoiler.getInstance();
+
+            Console.WriteLine("isEmpty: {0} ", boiler.IsEmpty());
+            boiler.Fill();
+
+            Console.WriteLine("===========================================");
+            
+            // Vì là một đối tượng duy nhất nên IsEmpty đã bị thay đổi thành False sau phương thức Fill() của boiler1
+            Console.WriteLine("isEmpty: {0}", boiler2.IsEmpty());
+            boiler2.Fill();
+
+            Console.WriteLine("===========================================");
+
+            // Boiler 1
+            boiler.Boil();
+
+            Console.WriteLine("===========================================");
+
+            // Boiler 2
+            boiler2.Drain();
+
+            Console.ReadKey();
+        }
+    }
+}

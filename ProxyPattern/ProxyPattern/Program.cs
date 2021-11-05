481bb7a343e2aea0fe1e6e23ccdbb895a794cc1c
ndTh4ng <thangxacam@gmail.com>
2021-11-05 11:18:34 +0700

    .

---
+++ ProxyPattern/ProxyPattern/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyPattern
+{
+    abstract class Subject
+    {
+        public abstract void DoSomeWork();
+    } 
+
+    class ConcreteSubject : Subject
+    {
+        public override void DoSomeWork()
+        {
+            Console.WriteLine("TIen hanh yeu cau");
+        }
+    }
+
+    class Proxy : Subject
+    {
+        Subject subject;
+
+        public Proxy()
+        {
+            subject = null;
+        }
+
+        public Proxy(Subject subject)
+        {
+            this.subject = subject;
+        }
+
+        public override void DoSomeWork()
+        {
+            Console.WriteLine("Hello!");
+            if (subject == null)
+            {
+                subject = new ConcreteSubject();
+            }
+            subject.DoSomeWork();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****************DEMO Proxy********************");
+            Subject proxy = new Proxy();
+            proxy.DoSomeWork();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TemplateMethodPattern/TemplateMethodPattern/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethodPattern
+{
+    class TwelveGrain : Bread
+    {
+        public override void MixIngredients()
+        {
+            Console.WriteLine("Gathering Ingredients for 12-Grain Bread.");
+        }
+        public override void Bake()
+        {
+            Console.WriteLine("Baking the 12-Grain Bread. (25 minutes)");
+        }
+    }
+
+    class Sourdough : Bread
+    {
+        public override void MixIngredients()
+        {
+            Console.WriteLine("Gathering Ingredients for Sourdough Bread.");
+        }
+        public override void Bake()
+        {
+            Console.WriteLine("Baking the Sourdough Bread. (20 minutes)");
+        }
+    }
+
+    class WholeWheat : Bread
+    {
+        public override void MixIngredients()
+        {
+            Console.WriteLine("Gathering Ingredients for Whole Wheat Bread.");
+        }
+
+        public override void Bake()
+        {
+            Console.WriteLine("Baking the Whole Wheat Bread. (15 minutes)");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sourdough sourdough = new Sourdough();
+            sourdough.Make();
+            TwelveGrain twelveGrain = new TwelveGrain();
+            twelveGrain.Make();
+            WholeWheat wholeWheat = new WholeWheat();
+            wholeWheat.Make();
+            Console.ReadKey();
+        }
+    }
+}
